cbff1a3853896b9f7203fc0476509cb231fd814d
JonasLow <147266737+JonasLow@users.noreply.github.com>
2024-08-21 01:50:18 +0800

    Problem Set 1 1. distinct prime 2. dot product 3. fibonacci numbers

---
+++ pset1/DotProduct.java
@@ -0,0 +1,19 @@
+import java.util.stream.*;
+
+public class DotProduct {
+	public static IntStream dot(int a, int b) {
+		return IntStream.rangeClosed(a, b)
+	    	.flatMap(x -> IntStream.rangeClosed(a, b)
+	    		.map(y -> x * y));
+	}
+
+
+	record Pair<T, U>(T t, U u) {}
+	
+	public static Stream<Pair> product(int a, int b) {
+		return IntStream.rangeClosed(a, b).
+			boxed().
+			flatMap(x -> IntStream.rangeClosed(a, b).
+				mapToObj(y -> new Pair<Integer, Integer>(x, y)));
+	}
+}
