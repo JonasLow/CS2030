c29d0408312160349105b2cee721e10396203d83
JonasLow <147266737+JonasLow@users.noreply.github.com>
2024-08-21 01:03:06 +0800

    Problem Set 1

---
+++ pset1/distinctPrime.java
@@ -0,0 +1,17 @@
+import java.util.stream.IntStream;
+
+public class distinctPrime {
+	static boolean isPrime(int n) {
+		return n > 1 && IntStream.range(2, n).
+			noneMatch(x -> n % x == 0);
+	}
+
+
+	public static IntStream omega(int n) {
+		return IntStream.rangeClosed(1, n).
+			map(x -> (int) IntStream.rangeClosed(1, x).
+				filter(y -> x % y == 0).
+				filter(y -> isPrime(y)).
+				count());
+	}
+}
