81494167652f8251a5640aeea218c6812ca57d98
JonasLow <147266737+JonasLow@users.noreply.github.com>
2024-08-26 15:28:47 +0800

    made some changes for correctness

--- pset1/FibonacciSequence.java
+++ pset1/FibonacciSequence.java
@@ -3,8 +3,8 @@ import java.util.stream.*;
 public class FibonacciSequence {
 	public static Stream<Integer> fibonacci(int n) {
 		record Pair<T, U>(T t, U u) {}
-		return Stream.iterate(new Pair<Integer, Integer>(1, 1), pair -> new Pair<Integer, Integer>(pair.u, pair.t + pair.u)).
-			limit(n).
-			map(pair -> pair.t);
+		return Stream.iterate(new Pair<Integer, Integer>(1, 1), pair -> new Pair<Integer, Integer>(pair.u, pair.t + pair.u))
+			.limit(n)
+			.map(pair -> pair.t);
 	}
 }
